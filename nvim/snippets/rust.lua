56efbe1bb5f11796c46118a6220db19812d0f992
Elio Galdino <eliogaldino79@outlook.com>
2024-05-11 19:14:15 -0300

    atualizacoes

--- nvim/snippets/rust.lua
+++ nvim/snippets/rust.lua
@@ -8,6 +8,8 @@ local fmta = require("luasnip.extras.fmt").fmta
 
 local snippets, autosnippets = {}, {}
 
+local bevy_import = s('bvy', t('use bevy::prelude::*;'))
+
 local bevy_component = s('comp', fmta([[
 #[derive(Component, Debug)]
 pub struct <> {
@@ -15,13 +17,27 @@ pub struct <> {
 }
 ]], { i(1), i(2) }))
 
+local bevy_bundle = s('bund', fmta([[
+#[derive(Bundle)]
+pub struct <>Bundle {
+    <>
+}
+]], { i(1), i(2) }))
+
+local bevy_resource = s('ress', fmta([[
+#[derive(Resource, Debug, Default)]
+pub struct <>Resource {
+    <>
+}
+]], { i(1), i(2) }))
+
 local bevy_delta_time = s('dt', t("time.delta_seconds()"))
 
 local bevy_plugin = s('bpl', fmta([[
-struct <>;
+pub struct <>Plugin;
 
-pub impl Plugin for <> {
-  fn build(app: App){
+impl Plugin for <>Plugin {
+  fn build(&self, app: &mut App){
     <>
   }
 }
@@ -30,5 +46,11 @@ pub impl Plugin for <> {
 local bevy_query = s('bq', fmta('query: Query<>', { i(1) }))
 
 table.insert(autosnippets, bevy_component)
+table.insert(autosnippets, bevy_import)
+table.insert(autosnippets, bevy_delta_time)
+table.insert(autosnippets, bevy_plugin)
+table.insert(autosnippets, bevy_bundle)
+table.insert(autosnippets, bevy_resource)
+
 
 return snippets, autosnippets
